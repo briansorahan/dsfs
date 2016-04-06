010e49625578e2637ab3570852ac85dba29a2ddf
Brian Sorahan <bsorahan@leaf.ag>
2016-04-06 07:12:42 -0500

    working on sqlite challenge

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 IMAGE           = bsorahan/tds
+IPYTHON         = /opt/conda/bin/ipython
+PYTHON2         = /opt/conda/bin/python2.7
 
 # default target
 all             : image
 
 clean		:
 		@rm -rf .image
+		@docker rm -f weather_c
 
 image		: .image
 .image		: Dockerfile
@@ -12,9 +15,14 @@ image		: .image
 		@touch $@
 
 py              : image
-		@docker run -it $(IMAGE) /opt/conda/bin/ipython
+		@docker run -it $(IMAGE) $(PYTHON)
 
 sh              : image
 		@docker run -it $(IMAGE) /bin/bash
+
+weather         :
+.weather        : image
+		@cat database.py | docker run -i --name weather_c $(IMAGE) $(PYTHON2)
+		@docker cp weather_c:/projects/getting_started.db .
 
 .PHONY		: all clean py sh
--- database.py
+++ database.py
@@ -1,17 +1,42 @@
 import sqlite3 as lite
+import pandas  as pd
 
-con = lite.connect('getting_started.db')
+con = lite.connect("getting_started.db")
+
+cities = (("New York City", "NY"),
+          ("Boston", "MA"),
+          ("Chicago", "IL"),
+          ("Miami", "FL"),
+          ("Dallas", "TX"),
+          ("Seattle", "WA"),
+          ("Portland", "OR"),
+          ("San Francisco", "CA"),
+          ("Los Angeles", "CA"))
+
+weather = (("New York City",   2013,    "July",        "January",     62),
+           ("Boston",          2013,    "July",        "January",     59),
+           ("Chicago",         2013,    "July",        "January",     59),
+           ("Miami",           2013,    "August",      "January",     84),
+           ("Dallas",          2013,    "July",        "January",     77),
+           ("Seattle",         2013,    "July",        "January",     61),
+           ("Portland",        2013,    "July",        "December",    63),
+           ("San Francisco",   2013,    "September",   "December",    64),
+           ("Los Angeles",     2013,    "September",   "December",    75))
 
 # Inserting rows by passing values directly to `execute()`
 with con:
     cur = con.cursor()
-    weather = (('New York City',   2013,    'July',        'January',     62),
-               ('Boston',          2013,    'July',        'January',     59),
-               ('Chicago',         2013,    'July',        'January',     59),
-               ('Miami',           2013,    'August',      'January',     84),
-               ('Dallas',          2013,    'July',        'January',     77),
-               ('Seattle',         2013,    'July',        'January',     61),
-               ('Portland',        2013,    'July',        'December',    63),
-               ('San Francisco',   2013,    'September',   'December',    64),
-               ('Los Angeles',     2013,    'September',   'December',    75))
-    cur.executemany('INSERT INTO WEATHER VALUES (?, ?, ?, ?, ?)', weather)
+
+    # Create the tables.
+    cur.execute("DROP TABLE cities")
+    cur.execute("DROP TABLE weather")
+    cur.execute("CREATE TABLE cities (name text, state text)")
+    cur.execute("CREATE TABLE weather (city text, year integer, warm_month text, cold_month text, average_high integer)")
+
+    cur.executemany("INSERT INTO cities  VALUES (?, ?)", cities)
+    cur.executemany("INSERT INTO WEATHER VALUES (?, ?, ?, ?, ?)", weather)
+
+    cur.execute("SELECT c.name || ', ' || c.state FROM city c INNER JOIN weather w ON c.name = w.city WHERE lower(w.warm_month) = 'july'")
+    
+    frame = pd.DataFrame(cur.fetchall())
+    print("The cities that are warmest in July are: " + ", ".join(frame))
