50ca1d400a387afa4ba76ec90d1581ce8146322b
Brian Sorahan <bsorahan@leaf.ag>
2016-05-23 09:10:55 -0500

    update dockerfile, compute 3 metrics for loan data

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
-IMAGE           = bsorahan/tds
+IMAGE           = bsorahan/ipynb
 IPYTHON         = /opt/conda/bin/ipython
 PYTHON2         = /opt/conda/bin/python2.7
 
 # default target
+notebook        :
+		@docker run --rm -it -p 8888:8888 -v $(shell pwd)/notebooks:/home/jovyan/work $(IMAGE)
+
 all             : image
 
 clean		:
@@ -13,17 +16,10 @@ image		: .image
 		@docker build -t $(IMAGE) . >/dev/null 2>/dev/null
 		@touch $@
 
-jupyter         :
-		@docker run --rm -it -p 8888:8888 -v $(pwd):/notebooks jupyter/notebook
-
 py              : image
 		@docker run -it $(IMAGE) $(PYTHON)
 
 sh              : image
 		@docker run -it $(IMAGE) /bin/bash
 
-weather         : .image database.py
-		@docker rm -f weather_c >/dev/null 2>/dev/null || true
-		@cat database.py | docker run -i --name weather_c $(IMAGE) $(PYTHON2)
-
-.PHONY		: all clean jupyter py sh weather
+.PHONY		: all clean jupyter py sh
---
+++ loans_metrics.py
@@ -0,0 +1,25 @@
+import pandas                  as pd
+import numpy                   as np
+import statsmodels.api         as sm
+import statsmodels.formula.api as smf
+
+from patsy                    import dmatrices
+from sklearn.cross_validation import KFold
+
+df = pd.read_csv("https://raw.githubusercontent.com/Thinkful-Ed/curric-data-001-data-sets/master/loans/loansData.csv")
+
+df['home_ownership'] = df['Home.Ownership']
+df['int_rate'] = df['Interest.Rate'].map(lambda val: float(val[:-1]))
+df['annual_inc'] = df['Monthly.Income']*12
+
+# print('\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> int_rate ~ annual_inc >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n')
+est1 = smf.ols(formula='int_rate ~ annual_inc', data=df).fit()
+print(est1.summary())
+
+# print('\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> int_rate ~ annual_inc + home_ownership >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n')
+# est2 = smf.ols(formula='int_rate ~ annual_inc + home_ownership', data=df).fit()
+# print(est2.summary())
+
+# print('\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> int_rate ~ annual_inc * home_ownership >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n')
+# est3 = smf.ols(formula='int_rate ~ annual_inc * home_ownership', data=df).fit()
+# print(est3.summary())
